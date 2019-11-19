14c693440b23bf90019f96f07ffda35d0d408e1a
Sabin Marcu <sabin.marcu@rga.com>
2019-11-19 17:46:25 +0200

    Fix base for github pages

---
+++ packages/slides/scripts/fixbase.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const origin = require('remote-origin-url');
+
+const indexPath = path.resolve(__dirname, "../public/index.html");
+const content = fs.readFileSync(
+  indexPath,
+  "utf-8",
+);
+
+const gitPath = path.resolve(__dirname, "../../../.git/config");
+const { 
+  1: account,
+  2: repo,
+} = origin.sync(gitPath).match(/:([^\/]+)\/([^\.]+)\./);
+
+fs.writeFileSync(
+  indexPath,
+  content
+    .replace(/<head>/, `<head>
+    <base href="https://${account}.github.io/${repo}/">
+  `),
+);
