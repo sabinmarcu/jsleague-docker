601146343187d0686c9c994d1dc14be402bcdd78
Sabin Marcu <sabin.marcu@rga.com>
2019-11-19 18:00:59 +0200

    Fixed js paths for github pages

--- packages/slides/scripts/fixbase.js
+++ packages/slides/scripts/fixbase.js
@@ -2,23 +2,29 @@ const fs = require('fs');
 const path = require('path');
 const origin = require('remote-origin-url');
 
-const indexPath = path.resolve(__dirname, "../public/index.html");
+const publicPath = path.resolve(__dirname, '../public');
+
+// index.html
+const indexPath = path.resolve(publicPath, "index.html");
 const content = fs.readFileSync(
   indexPath,
   "utf-8",
 );
 
-const gitPath = path.resolve(__dirname, "../../../.git/config");
-const { 
-  1: account,
-  2: repo,
-} = origin.sync(gitPath).match(/:([^\/]+)\/([^\.]+)\./);
-
 fs.writeFileSync(
   indexPath,
   content
-    .replace(/(href|src)="\//g, '$1="')
-    .replace(/<head>/, `<head>
-    <base href="https://${account}.github.io/${repo}/">
-  `),
+    .replace(/(href|src)="\//g, '$1="'),
 );
+
+// app.js files
+fs.readdirSync(publicPath)
+  .filter(it => it.includes('app'))
+  .forEach(file => {
+    const src = path.resolve(publicPath, file);
+    const cont = fs.readFileSync(src, 'utf-8');
+    fs.writeFileSync(
+      src,
+      cont.replace(/\/([a-zA-Z-]+-data)/g, "$1")
+    );
+  })
